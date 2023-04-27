9d9be1c9c8358a251547fb77eaac9e9a64d2bc04
Yereeeeee <yerlan.makhmetaliyev@narxoz.kz>
2023-04-27 12:54:37 +0600

    БАОЖ 10

--- kolesa/settings.py
+++ kolesa/settings.py
@@ -124,3 +124,10 @@ LOGOUT_REDIRECT_URL = 'index'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': os.path.join(BASE_DIR, 'cashes')
+    }
+}
--- kolesa/urls.py
+++ kolesa/urls.py
@@ -3,14 +3,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
-
+from django.views.decorators.cache import cache_page
 from car.views import *
 from kolesa import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('captcha/', include('captcha.urls')),
-    path('', IndexView.as_view(), name='index'),
+    path('', cache_page(60)(IndexView.as_view()), name='index'),
     path('register/', RegisterView.as_view(), name="register"),
     path('login/', Login.as_view(), name="login"),
     path('logout/', sign_out, name="logout"),
